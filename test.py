ff76c8566d89d920e396ad27e3ea4237f32b2aa9
sudharshinit <sudharshini22.thulasi@gmail.com>
2026-07-18 19:40:47 +0530

    modified test.py,paths

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-this is the file im tryna commit again
+import math 
+x=[]
+y=[]
+a=[2,3,4]
+b=[0,2,2]
+for i in range(0,len(a)-1):  
+ m = math.gcd(a[i],a[i+1])  
+ x.append(m)
+print(x)
+x.sort()
+print("Sorted x:", x)
+for i in range(len(b)) :
+ k = x[b[i]]
+ y.append(k) 
+print(y)
+
+
